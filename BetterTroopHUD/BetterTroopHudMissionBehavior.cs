cd3e23bec8f2505a8cab75e01b70239dc3f3233a
Haarrdy <141058791+Haarrdy@users.noreply.github.com>
2023-08-21 22:45:02 +0200

    v1.0.1

---
+++ BetterTroopHUD/BetterTroopHudMissionBehavior.cs
@@ -0,0 +1,105 @@
+﻿using TaleWorlds.Core;
+using TaleWorlds.Engine.GauntletUI;
+using TaleWorlds.MountAndBlade.GauntletUI.Mission;
+using TaleWorlds.ScreenSystem;
+using static BetterTroopHUD.Utils;
+
+namespace BetterTroopHUD;
+
+public class BetterTroopHudMissionBehavior : MissionGauntletBattleUIBase
+{
+    private GauntletLayer? _gauntletLayer;
+    private BetterTroopHudVM? _dataSource;
+
+    public override void EarlyStart()
+    {
+        base.EarlyStart();
+        DisplayDebugMessage("[DEBUG] EarlyStart: called");
+        
+        _dataSource = new BetterTroopHudVM(Mission);
+        _gauntletLayer = new GauntletLayer(1);
+        _gauntletLayer.LoadMovie("BetterTroopHUD", _dataSource);
+        ScreenManager.TopScreen.AddLayer(_gauntletLayer);
+    }
+
+    public override void AfterStart()
+    {
+        base.AfterStart();
+        
+        DisplayDebugMessage("[DEBUG] AfterStart: called");
+        
+        _dataSource?.Initialize();
+    }
+    
+    protected override void OnCreateView() => _dataSource.IsAgentStatusAvailable = true;
+    protected override void OnDestroyView() => _dataSource.IsAgentStatusAvailable = false;
+
+    // public override void OnMissionScreenInitialize()
+    // {
+    //     base.OnMissionScreenInitialize();
+    //     
+    //     DisplayDebugMessage("[DEBUG] OnMissionScreenInitialize: called");
+    //     
+    //     // todo Implement _isInDeployment
+    //     // this._isInDeployement = base.Mission.GetMissionBehavior<BattleDeploymentHandler>() != null;
+    //     // if (this._isInDeployement)
+    //     // {
+    //     //     this._deploymentMissionView = base.Mission.GetMissionBehavior<DeploymentMissionView>();
+    //     //     if (this._deploymentMissionView != null)
+    //     //     {
+    //     //         DeploymentMissionView deploymentMissionView = this._deploymentMissionView;
+    //     //         deploymentMissionView.OnDeploymentFinish = (OnPlayerDeploymentFinishDelegate)Delegate.Combine(deploymentMissionView.OnDeploymentFinish, new OnPlayerDeploymentFinishDelegate(this.OnDeploymentFinish));
+    //     //     }
+    //     // }
+    // }
+    
+    public override void OnMissionScreenFinalize()
+    {
+        base.OnMissionScreenFinalize();
+        
+        DisplayDebugMessage("[DEBUG] OnMissionScreenFinalize: called");
+        
+        // Clean up
+        ScreenManager.TopScreen.RemoveLayer(_gauntletLayer);
+        _gauntletLayer = null;
+        _dataSource?.OnFinalize();
+        _dataSource = null;
+    }
+
+    // private void OnDeploymentFinish()
+    // {
+    //     this._isInDeployement = false;
+    //     DeploymentMissionView deploymentMissionView = this._deploymentMissionView;
+    //     deploymentMissionView.OnDeploymentFinish = (OnPlayerDeploymentFinishDelegate)Delegate.Remove(deploymentMissionView.OnDeploymentFinish, new OnPlayerDeploymentFinishDelegate(this.OnDeploymentFinish));
+    // }
+    
+    public override void OnMissionModeChange(MissionMode oldMissionMode, bool atStart)
+    {
+        base.OnMissionModeChange(oldMissionMode, atStart);
+        _dataSource?.OnMissionModeChange(oldMissionMode, atStart);
+    }
+
+    public override void OnMissionScreenTick(float dt)
+    {
+        base.OnMissionScreenTick(dt);
+        
+        // _dataSource?.IsInDeployment = _isInDeployment; // todo
+        _dataSource?.Tick(dt);
+    }
+
+    public override void OnPhotoModeActivated()
+    {
+        base.OnPhotoModeActivated();
+        
+        // Hide UI
+        _gauntletLayer._gauntletUIContext.ContextAlpha = 0f;
+    }
+
+    public override void OnPhotoModeDeactivated()
+    {
+        base.OnPhotoModeDeactivated();
+        
+        // Un-hide UI
+        _gauntletLayer._gauntletUIContext.ContextAlpha = 1f;
+    }
+}
---
+++ README.md
@@ -0,0 +1,26 @@
+# BetterTroopHUD
+
+<p align="center">
+    <a href="#" alt="Steam update date">
+        <img src="https://img.shields.io/steam/update-date/2999591774?style=for-the-badge&logo=steam" />
+    </a>
+    <img alt="Lines of code" src="https://img.shields.io/tokei/lines/github/haarrdy/MB_BetterTroopHUD?style=for-the-badge&logo=github">
+    <img alt="Scrutinizer code quality (GitHub/Bitbucket)" src="https://img.shields.io/scrutinizer/quality/g/haarrdy/MB_BetterTroopHUD?style=for-the-badge&logo=github">
+    <a href="https://steamcommunity.com/sharedfiles/filedetails/?id=2999591774" alt="Steam workshop page">
+        <img alt="Steam downloads" src="https://img.shields.io/steam/downloads/2999591774?style=for-the-badge&logo=steam" />
+    </a>
+    <a href="https://steamcommunity.com/sharedfiles/filedetails/?id=2999591774" alt="Steam workshop page">
+        <img alt="Steam subscriptions" src="https://img.shields.io/steam/subscriptions/2999591774?style=for-the-badge&logo=steam" />
+    </a>
+    <a href="https://steamcommunity.com/sharedfiles/filedetails/?id=2999591774" alt="Steam workshop page">
+        <img alt="Steam views" src="https://img.shields.io/steam/views/2999591774?style=for-the-badge&logo=steam" />
+    </a>
+    <a href="https://steamcommunity.com/sharedfiles/filedetails/?id=2999591774" alt="Steam workshop page">
+        <img alt="Steam size" src="https://img.shields.io/steam/size/2999591774?style=for-the-badge&logo=steam" />
+    </a>
+
+
+    <a href="#" alt="preview">
+        <img src="../Assets/Mod%20preview%20small.png" />
+    </a>
+</p>
